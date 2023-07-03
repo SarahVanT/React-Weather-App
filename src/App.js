ab11a5fabb2ac107f6f2d246ac9a79754e994f22
SarahVanT <sarahvantilburg1@outlook.com>
2023-07-03 13:35:17 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import './App.css';
-import Search from "./Search";
-import Footer from "./Footer";
 
-export default function Weather() {
-  // let weatherData = {
-  //   city: "New York",
-  //   temperature: 19,
-  //   date: "Tuesday 10:00",
-  //   description: "Cloudy",
-  //   imgUrl: "http://openweathermap.org/img/wn/50d@2x.png",
-  //   humidity: 80,
-  //   wind: 10,
-  // };
 
+export default function App() {
   return (
-    <div className='App container'>
-      <div className='weather-app-wrapper'>
-        <div className='weather-app'>
-          <Search />
-          
-        </div>
-        <Footer />
+    <div className='App'>
+      <div className='container'>
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by <a href='https://joyful-tiramisu-ce3d71.netlify.app/' target="_blank" rel="noreferrer"> Sarah VanTilburg</a> and is <a
+          href="https://github.com/SarahVanT/React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+          >open sourced on GitHub</a>
+        </footer>
       </div>
     </div>
   );
