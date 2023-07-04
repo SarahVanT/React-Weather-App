d146dc1f9dd03574895d11c8b9041de831a603ed
SarahVanT <sarahvantilburg1@outlook.com>
2023-07-04 12:53:40 -0400

    Added date on weather component

---
+++ src/FormattedDate.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function FormattedDate(props){
+    // This function adds a leading zero to a number if it is less than 10
+    function addZero(number){
+        if (number < 10) {
+            return `0${number}`;
+        } else {
+            return number;
+        }
+    }
+
+    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = weekday[props.date.getDay()];
+    let hours = addZero(props.date.getHours());
+    let minutes = addZero(props.date.getMinutes());
+    let meridiem  = "";
+
+    if (hours >= 12){
+        meridiem  += "PM";
+    } else{
+        meridiem  += "AM";
+    }
+
+    return <div>{day} {hours}:{minutes} {meridiem }</div>;
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props){
@@ -13,8 +14,8 @@ export default function Weather(props){
             ready: true,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
-            city: response.data.main.name,
-            date: "Monday 6:00",
+            city: response.data.name,
+            date: new Date(response.data.dt*1000),
             description: response.data.weather[0].description,
             icon: response.data.weather[0].icon,
             wind: response.data.wind.speed,
@@ -54,7 +55,8 @@ export default function Weather(props){
                 </form>
                 <h1>Dayton</h1>
                 <ul>
-                    <li>{weatherData.date}</li>
+                    {/* Calling component, sending date from timestamp */}
+                    <li><FormattedDate date={weatherData.date}/></li>
                     <li className="text-capitalized">{weatherData.description}</li>
                 </ul>
                 <div className="row mt-3">
